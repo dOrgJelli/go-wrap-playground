f7aa39b17a92c0925472edb9e6c96ec1ecc967b8
dOrgJelli <jelli@dorg.tech>
2023-07-14 04:39:33 +0200

    chore: misc cleanup

--- module/module.go
+++ module/module.go
@@ -9,11 +9,9 @@ func CalcPriceImpactWithAmount(args *wrap.ArgsCalcPriceImpactWithAmount) string 
 	spotPriceBefore, _ := types.NewDecFromStr(args.SpotPriceBefore)
 	priceImpact, _ := types.NewDecFromStr(args.PriceImpact)
 
-	// Not working for some reason
-	// one := types.NewDecFromInt(types.NewInt(1)) 
 	oneDec, _ := types.NewDecFromStr("1")
 	newImpact := priceImpact.Add(oneDec)
 	effectivePrice := newImpact.Mul(spotPriceBefore)
-  tokenAmount, _ := types.NewDecFromStr(args.TokenAmount)
+	tokenAmount, _ := types.NewDecFromStr(args.TokenAmount)
 	return tokenAmount.Quo(effectivePrice).String()
 }
